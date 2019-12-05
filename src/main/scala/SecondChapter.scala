a5de54f9b12d0a18a25eca2ceb391b1c84dfbf67
Sandeep Verma <sandeep201187@gmail.com>
2019-12-05 16:30:52 +0700

    compose

--- src/main/scala/SecondChapter.scala
+++ src/main/scala/SecondChapter.scala
@@ -30,4 +30,12 @@ object SecondChapter extends App {
   // Exercise 2.4
   def uncurry[A,B,C](f: A => B => C): (A, B) => C =
     (a: A, b: B) => f(a)(b)
+
+  // Exercise 2.5
+  def compose[A,B,C](f: B => C, g: A => B): A => C =
+    (a: A) => f(g(a))
+
+  val f = (x: Double) => math.Pi / 2 - x
+  val cos = f andThen math.sin
+  println(cos(math.Pi / 6))
 }
