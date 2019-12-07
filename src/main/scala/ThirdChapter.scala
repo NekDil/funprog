1e4650d774079170839836f64eb72570fea3af71
Sandeep Verma <sandeep201187@gmail.com>
2019-12-07 13:49:55 +0700

    folds

--- src/main/scala/ThirdChapter.scala
+++ src/main/scala/ThirdChapter.scala
@@ -1,3 +1,4 @@
+
 import scala.annotation.tailrec
 
 object ThirdChapter extends App {
@@ -47,6 +48,11 @@ object ThirdChapter extends App {
       case Cons(h, t) => Cons(h, append(t, b))
     }
 
+    def append[A](a: List[A], e: A): List[A] = a match {
+      case Nil => Cons(e, Nil)
+      case Cons(h, t) => Cons(h, append(t, e))
+    }
+
     // Exercise 3.6
     def init[A](l: List[A]): List[A] = l match {
       case Nil => sys.error("Nil list have no elements")
@@ -54,26 +60,90 @@ object ThirdChapter extends App {
       case Cons(h, t) => Cons(h, init(t))
     }
 
+    def dropWhile2[A](l: List[A])(f: A => Boolean): List[A] = l match {
+      case Cons(h, t) if(f(h)) => dropWhile2(t)(f)
+      case _ => l
+    }
+
+    def foldRight[A,B](as: List[A], z: B)(f: (A, B) => B): B = as match {
+      case Nil => z
+      case Cons(x, xs) => f(x, foldRight(xs, z)(f))
+    }
+
+    // foldRight from Standard Library
+    def head[A](l: List[A]): A = l match {
+      case Nil => sys.error("head of a Nil list")
+      case Cons(h, _) => h
+    }
+
+    def isEmpty[A](l: List[A]): Boolean = l match {
+      case Nil => true
+      case Cons(_,_) => false
+    }
+
+    def reverseIt[A](l: List[A]): List[A] = {
+      var result: List[A] = Nil
+      var these: List[A] = l
+      while (!isEmpty(these)) {
+        result = Cons(head(these), result)
+        these = tail(these)
+      }
+      result
+    }
+
+    def foldRightIt[A,B](as: List[A], z: B)(op: (A, B) => B): B = {
+      var acc = z
+      var these = reverseIt(as)
+      while (!isEmpty(these)) {
+        acc = op(head(these), acc)
+        these = tail(these)
+      }
+      acc
+    }
+
+    // Exercise 3.7
+    // Not possible
+
+    // Exercise 3.8
+    // Gets same list
+
+    // Exercise 3.9
+    def length[A](l: List[A]): Int = foldRight(l, 0)((_: A, a: Int) => a + 1)
+
+    def sum2(l: List[Int]): Int = foldRight(l, 0)(_ + _)
+    def product2(l: List[Double]): Double = foldRight(l, 1.0)(_ * _)
+    def sumIt(l: List[Int]): Int = foldRightIt(l, 0)(_ + _)
+    def productIt(l: List[Double]): Double = foldRightIt(l, 1.0)(_ * _)
   }
+  import ThirdChapter.List._
 
   // Exercise 3.1
   val x = List(1, 2, 3, 4, 5) match {
     case Cons(x, Cons(2, Cons(4, _))) => x
     case Nil => 42
     case Cons(x, Cons(y, Cons(3, Cons(4, _)))) => x + y
-    case Cons(h, t) => h + List.sum(t)
+    case Cons(h, t) => h + sum(t)
     case _ => 101
   }
 
   println(x)
-
   val aList = List(1,2,3,4,5)
   val bList = List(1,2,3,4,5,6,7)
+  val dList: List[Double] = List(1,2,3,4,5)
 
-  println(List.tail(aList))
-  println(List.setHead(aList, 0))
-  println(List.drop(aList, 3))
-  println(List.dropWhile(bList, (x: Int) => x % 5 != 0))
-  println(List.append(aList, bList))
-  println(List.init(aList))
+
+
+  println(tail(aList))
+  println(setHead(aList, 0))
+  println(drop(aList, 3))
+  println(dropWhile(bList, (x: Int) => x % 5 != 0))
+  println(dropWhile2(bList)(x => x < 4))
+  println(append(aList, bList))
+  println(init(aList))
+  println(sumIt(aList))
+  println(productIt(dList))
+  println(reverseIt(aList))
+  println(foldRight(aList, Nil:List[Int])(Cons(_, _)))
+  println(length(bList))
+
 }
---
+++ target/scala-2.13/classes/worksheet.sc
@@ -0,0 +1,4 @@
+List(1,2,3).foldRight(0)(_ + _)
+List(1,2,3).drop(3)
+List(1,2,3).dropWhile(_ < 2)
+List(1,2,3).foldLeft(0)(_ + _)
